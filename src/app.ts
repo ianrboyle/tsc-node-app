ce9e8d82c91f10d80eb81d2bcb3ba57ba8f917fd
Ian Boyle <irboyle21@gmail.com>
2022-01-23 13:09:26 -0500

    imports express, makes simple send / listen request

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-const greeting: string = "hello"
+import express from "express"
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send('Hello')
+})
+
+app.listen(3000, () => console.log("server running") )
